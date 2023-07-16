420732e4bef95aa13e483635882bbe2dc2a5edb4
mh-ahmad <ahmad@momenthouse.com>
2023-07-17 03:25:52 +0500

    removed the client path

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require("path");
 
 const purchaseOrderRouter = require("./routes");
 
@@ -10,15 +9,7 @@ const PORT = process.env.PORT || 3002;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "../../client/build")));
-
 // Purchase Order routes
 app.use("/api", purchaseOrderRouter());
-
-// For any other routes, serve the React app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
-});
 
 app.listen(PORT, () => console.log(`Server is listening at port ${PORT}`));
